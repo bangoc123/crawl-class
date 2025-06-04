7969bc625d45e3b38e2e4ac8c60cd8bac4bb3e31
Ngoc Ba <bangoc.gdg@gmail.com>
2025-06-04 10:43:40 +0700

    Update readme

--- simple-crawl-python-requests/simple-crawl.py
+++ simple-crawl-python-requests/simple-crawl.py
@@ -17,22 +17,26 @@ soup = BeautifulSoup(response.text, "html.parser")
 # print("Tiêu đề trang:", title.text.strip() if title else "Không tìm thấy")
 # print("Đoạn văn:", paragraph.text.strip() if paragraph else "Không tìm thấy")
 
-# Tìm tất cả
+# Tìm tất cả các thẻ <p>
 all_paragraphs = soup.find_all("p")
 # for p in all_paragraphs:
 #     print(p.text.strip())
 #     print("--------------------------------")
 
+# Tìm thẻ <p> cuối cùng
 p_with_a = all_paragraphs[-1]
 
 print('--p_with_a', p_with_a)
 
+# Tìm thẻ <a> bên trong thẻ <p> cuối cùng
 a_tag = p_with_a.find("a")
 
 print('--a_tag', a_tag)
 
+# Tìm nội dung của thẻ <a>
 print('--a_tag.text', a_tag.text)
 
+# Tìm đường dẫn của thẻ <a>
 print('--a_tag.get("href")', a_tag.get("href"))
 
 
